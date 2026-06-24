7077aa07b2281c1cc6f0c5cd9efbfd5ec9827220
LSton3y <leolstone@icloud.com>
2026-06-24 22:09:35 +0100

    Changed reset button size

--- src/ui/reset_button.py
+++ src/ui/reset_button.py
@@ -8,14 +8,14 @@ class ResetButton:
         self.rect = pygame.Rect(
             0,
             0,
-            src.constants.TOP_BAR_HEIGHT // 2,
-            src.constants.TOP_BAR_HEIGHT // 2
+            src.constants.TOP_BAR_HEIGHT // 1.5,
+            src.constants.TOP_BAR_HEIGHT // 1.5
         )
 
         # Centers rect to top middle
         self.rect.centerx = src.constants.WIDTH // 2
         self.rect.top = src.constants.TOP_BAR_HEIGHT // 5
-        
+
 
     def clicked(self, pos):
         return self.rect.collidepoint(pos)
